f0819d6118b574eafbc84b3ddaa49e8b04606a43
Andresf07b <bolanosandres43@gmail.com>
2025-05-06 11:57:16 -0500

    modify functions

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-inventory: list = []
+inventory: list = [{"nombre": "andres", "precio": 2000}]
 
 def add_product(products:list,name:str, price:float, amount:int)->list:
     product = {
@@ -9,7 +9,13 @@ def add_product(products:list,name:str, price:float, amount:int)->list:
     products.append(product)
     return products
 
-def get_product(name:str)-> dict:
-    print()
+def get_product(name:str, products: list)-> dict:
+    product = list(filter(lambda x: x["nombre"] == name, products))
+    return product[0] if product else {}
+
+
+
+# print(inventory)
+print(get_product("andres", inventory))
 
 
